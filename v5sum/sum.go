f0f16bdd19c00aeb48038b0892396d39d596d2ba
chenhao1123 <chenhao.1123@hotmail.com>
2018-08-07 19:11:21 +0800

    arrays&slices

--- v5sum/sum.go
+++ v5sum/sum.go
@@ -1,4 +1,4 @@
-package main
+package sum
 
 //Sum will take an array of numbers and return the total.
 func Sum(numbers []int) (sum int) {
@@ -8,6 +8,25 @@ func Sum(numbers []int) (sum int) {
 	return
 }
 
-func main() {
+//SumAll will take a varying number of slices, returning a new slice containing the totals for each slice pass in.
+func SumAll(numbersToSum ...[]int) (sums []int) {
 
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+//SumAllTails calculates the totals of the "tails" of each slice.
+func SumAllTails(numbersToSumTails ...[]int) (sums []int) {
+	for _, numbers := range numbersToSumTails {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+	return
 }
