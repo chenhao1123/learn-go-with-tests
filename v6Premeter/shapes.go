3f39c17c0e55e41bfea0c556b04a433eb3b01653
chenhao1123 <chenhao.1123@hotmail.com>
2018-08-11 21:08:35 +0800

    interface

--- v6Premeter/shapes.go
+++ v6Premeter/shapes.go
@@ -4,25 +4,41 @@ import (
 	"math"
 )
 
-//Rectangle test
+//Shape interface Area
+type Shape interface {
+	Area() float64
+}
+
+//Rectangle struct Width Height
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-//GetAera return aera
-func (r Rectangle) GetAera() float64 {
+//Area return area
+func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
 
-//Circle struct
+//Circle struct Radius
 type Circle struct {
 	Radius float64
 }
 
-//GetAera return aera
-func (r Circle) GetAera() float64 {
+//Area return area
+func (r Circle) Area() float64 {
 	return math.Pi * r.Radius * r.Radius
+}
+
+//Triangle struct Base Height
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+//Area return area
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
 
 // Perimeter return
