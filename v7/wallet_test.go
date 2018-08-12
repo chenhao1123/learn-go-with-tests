3adc6614849d2db2b215d7cdad2c865d58f86561
chenhao1123 <chenhao.1123@hotmail.com>
2018-08-12 15:05:40 +0800

    helper()

--- v7/wallet_test.go
+++ v7/wallet_test.go
@@ -32,6 +32,7 @@ func TestWallet(t *testing.T) {
 }
 
 func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
 
 	if got != want {
@@ -40,6 +41,7 @@ func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
 }
 
 func assertError(t *testing.T, got error, want error) {
+	t.Helper()
 	if got == nil {
 		t.Fatal("wanted an error bu didn't get one")
 	}
@@ -49,6 +51,7 @@ func assertError(t *testing.T, got error, want error) {
 	}
 }
 func assertNoError(t *testing.T, got error) {
+	t.Helper()
 	if got != nil {
 		t.Fatal("got an error but didnt want one")
 	}
